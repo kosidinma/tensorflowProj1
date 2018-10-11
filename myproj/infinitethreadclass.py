267d83105ee71e57b5107468dec33fec2bf893af
Kosidinma Onyenso <konyenso@tesla.com>
2018-10-10 17:59:53 -0700

    thread with input arg update

--- myproj/infinitethreadclass.py
+++ myproj/infinitethreadclass.py
@@ -3,26 +3,27 @@ from threading import Thread  # we're using threads
 
 # infinite thread class
 class InfiniteLoop(Thread):
-    def __init__(self, target):  # initialise variables and class input arguments
+    def __init__(self, target, width):  # initialise variables and class input arguments
         Thread.__init__(self)
         self.running = True
         self.target = target
+        self.width = width
 
     def run(self):
         while self.running:
-            self.target()
+            self.target(self.width)
 
     def stop(self):
         self.running = False
 
 
-def myloopFunction():
-    print("lol")
+def myloopFunction(width):
+    print("lol" + str(width))
 
 
 def main():
     # start timer
-    t = InfiniteLoop(myloopFunction)  # every 0.1s
+    t = InfiniteLoop(myloopFunction,1)  # every 0.1s
     t.start()
 
 
